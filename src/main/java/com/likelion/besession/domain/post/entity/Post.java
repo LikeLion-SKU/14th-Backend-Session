8ca2fe7f87b5be48db669004bff8205c56b3d412
김나경 <1030n@naver.com>
2026-03-03 11:26:35 +0900

    :sparkles: Feat: Post Entity 추가

---
+++ src/main/java/com/likelion/besession/domain/post/entity/Post.java
@@ -0,0 +1,30 @@
+package com.likelion.besession.domain.post.entity;
+
+import com.likelion.besession.global.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String content;
+}
