9336e9e8f53414857b9bb6cc66293214227c2644
shinchaerin79 <shinchaerin79@gmail.com>
2026-03-20 15:42:24 +0900

    :sparkles: Feat: Post Entity에 User 연관관계 추가

--- src/main/java/com/likelion/besession/domain/post/entity/Post.java
+++ src/main/java/com/likelion/besession/domain/post/entity/Post.java
@@ -1,12 +1,16 @@
 package com.likelion.besession.domain.post.entity;
 
-import com.likelion.besession.domain.post.dto.request.UpdatePostRequest;
+import com.likelion.besession.domain.user.entity.User;
 import com.likelion.besession.global.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,11 +21,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "post")
 public class Post extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
 
   @Column(nullable = false)
   private String title;
