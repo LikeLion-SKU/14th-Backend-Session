f1a313a9166f38f288e897c4b89e5bf1ac8b6342
이충환 <alsk3528@gmail.com>
2026-03-25 23:03:25 +0900

    :refactor: 원격 저장소에서 수정

--- src/main/java/com/likjelion/besession/Test.java
+++ src/main/java/com/likjelion/besession/Test.java
@@ -3,6 +3,6 @@ package com.likjelion.besession;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello world!!");
+        System.out.println("conflict accur??");
     }
 }
