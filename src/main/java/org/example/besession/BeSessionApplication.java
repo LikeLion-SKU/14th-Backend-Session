5853eb86a0edaae1fd6bcd272ad68b4b8ebdb847
김민호 <2022305011@skuniv.ac.kr>
2026-03-27 16:43:45 +0900

    :tada: start: 1주차 백엔드세션 과제

---
+++ src/main/java/org/example/besession/BeSessionApplication.java
@@ -0,0 +1,13 @@
+package org.example.besession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSessionApplication.class, args);
+    }
+
+}
