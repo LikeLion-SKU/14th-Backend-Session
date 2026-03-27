56e2b02eb004d0ebd175901caae83599c6c7a0e7
김민호 <2022305011@skuniv.ac.kr>
2026-03-27 16:46:09 +0900

    :recycle: Refactor: 원격 저장소에서 수정

--- src/main/java/org/example/besession/Test.java
+++ src/main/java/org/example/besession/Test.java
@@ -2,6 +2,6 @@ package org.example.besession;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Conflict occur?");
     }
 }
