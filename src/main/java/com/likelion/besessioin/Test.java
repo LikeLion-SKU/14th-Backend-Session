f59da1d1be6bfb7d6ffaf31254b6695298283f0f
{오지우} <ojing0116@skuniv.ac.kr>
2026-03-27 13:31:06 +0900

    :recycle: Refactor: 로컬 저장소에서 수정

--- src/main/java/com/likelion/besessioin/Test.java
+++ src/main/java/com/likelion/besessioin/Test.java
@@ -2,6 +2,6 @@ package com.likelion.besessioin;
 
 public class Test {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println("No conflict detected");
     }
 }
