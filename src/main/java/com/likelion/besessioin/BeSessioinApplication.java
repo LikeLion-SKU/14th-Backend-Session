045fd8fb0a5d03ba3841bfa66d4ced78699c9f7a
{오지우} <ojing0116@skuniv.ac.kr>
2026-03-27 13:26:15 +0900

    :tada: Start: Hello world

---
+++ src/main/java/com/likelion/besessioin/BeSessioinApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.besessioin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSessioinApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeSessioinApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/likelion/besessioin/BeSessioinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.besessioin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSessioinApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
