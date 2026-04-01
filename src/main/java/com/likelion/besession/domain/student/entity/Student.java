44efd4456480421ba2309a9bb5bea89b2d1c9683
Jeong mokjin <jungmokjin@gmail.com>
2026-04-01 23:52:00 +0900

    2주차 과제

---
+++ src/main/java/com/likelion/besession/domain/student/entity/Student.java
@@ -0,0 +1,38 @@
+package com.likelion.besession.domain.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String department;
+
+    private String studentNum;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getDepartment(){return  department;}
+
+    public String getStudentNum(){ return studentNum;}
+}
