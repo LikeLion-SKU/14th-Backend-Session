e73ad57c1e1bda7b7963a5b8905bad20926f7349
eunhasu03 <softabc12@gmail.com>
2026-04-09 20:04:30 +0900

    📝 Docs: 3주차 과제 제출
    ♻️ Refactor: 3주차 세션 실습
    
    ➕ Dependency/Plugin: Swagger 추가
    
    🔧 Settings: Swagger 설정

--- src/main/java/com/likelion/besession/domain/student/controller/StudentController.java
+++ src/main/java/com/likelion/besession/domain/student/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.likelion.besession.domain.student.controller;
 
 import com.likelion.besession.domain.student.entity.Student;
 import com.likelion.besession.domain.student.repository.StudentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Student API", description = "학생 관련 API")
 public class StudentController {
     private final StudentRepository studentRepository; // 리포지토리 객체 생성
 
@@ -18,6 +21,10 @@ public class StudentController {
         this.studentRepository = studentRepository;
     }
 
+    @Operation(
+            summary = "전체 학생 조회",
+            description = "모든 학생 정보를 조회합니다."
+    )
     @GetMapping("/students")
     public List<Student> findAll(){
         return studentRepository.findAll();
