5322726ab90193d3974daa14ee5a87418ad00ce5
yeyun0423 <pet2ytty@naver.com>
2026-04-10 20:13:53 +0900

    ✨ Feat: Swagger(OpenAPI) 설정 추가

--- src/main/java/com/example/besession/domain/student/controller/StudentController.java
+++ src/main/java/com/example/besession/domain/student/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.besession.domain.student.controller;
 
 import com.example.besession.domain.student.entity.Student;
 import com.example.besession.domain.student.repository.StudentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name="student API", description = "학생 관련 API")
 public class StudentController {
 
     private final StudentRepository studentRepository;
@@ -17,6 +20,11 @@ public class StudentController {
     public StudentController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+    @Operation(
+            summary ="전체 학생 조회",
+            description ="모든 학생 정보를 조회합니다."
+    )
 
     @GetMapping("/students")
     public List<Student> findAll(){
