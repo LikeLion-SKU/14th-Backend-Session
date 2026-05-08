a87a4b527089ef9593f468228048bf4ae4f75968
jeongneung <jeongneung2052@gmail.com>
2026-05-09 03:56:01 +0900

    week6 assignment

--- src/main/java/com/likelion/besession/domain/post/entity/Post.java
+++ src/main/java/com/likelion/besession/domain/post/entity/Post.java
@@ -1,6 +1,7 @@
 package com.likelion.besession.domain.post.entity;
 
 import com.likelion.besession.domain.post.dto.request.UpdatePostRequest;
+import com.likelion.besession.domain.user.entity.User;
 import com.likelion.besession.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -13,10 +14,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "post")
 public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Column(nullable = false)
     private String title;
