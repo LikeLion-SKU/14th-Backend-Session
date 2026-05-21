7726e2ab806afcb1c1341f3bea546c03437359fa
wjdah1028 <wjdah1028@naver.com>
2026-05-21 09:58:54 +0900

    :recycle: Refactor: Auth 도메인 유효성 검사 및 비즈니스 로직 예외 처리

---
+++ src/main/java/com/likelion/besession/domain/auth/service/AuthService.java
@@ -0,0 +1,43 @@
+package com.likelion.besession.domain.auth.service;
+
+import com.likelion.besession.domain.auth.dto.request.LoginRequest;
+import com.likelion.besession.domain.auth.dto.response.LoginResponse;
+import com.likelion.besession.domain.auth.exception.AuthErrorCode;
+import com.likelion.besession.global.exception.CustomException;
+import com.likelion.besession.global.security.CustomUserDetails;
+import com.likelion.besession.global.security.CustomUserDetailsService;
+import com.likelion.besession.global.security.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    public LoginResponse login(LoginRequest request) {
+
+        // email로 사용자 조회
+        CustomUserDetails userDetails =
+                (CustomUserDetails) customUserDetailsService.loadUserByUsername(request.getEmail());
+
+        // 입력한 비밀번호와 DB에 저장된 암호화 비밀번호 비교
+        if (!passwordEncoder.matches(request.getPassword(), userDetails.getPassword())) {
+            throw new CustomException(AuthErrorCode.PASSWORD_FAIL);
+        }
+
+        // 로그인 성공 시 Access Token 발급
+        String accessToken = jwtProvider.createAccessToken(userDetails);
+
+        // AccessToken을 응답으로 반환
+        return LoginResponse.builder()
+                .accessToken(accessToken)
+                .build();
+    }
+}
