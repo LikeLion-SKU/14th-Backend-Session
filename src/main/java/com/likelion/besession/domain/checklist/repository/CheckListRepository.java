e6353a7875711dbb87a2f7385d1f24d4361c1e9d
wjdah1028 <wjdah1028@naver.com>
2026-06-04 11:10:10 +0900

    :recycle: Refactor: 미완료 체크리스트 확인 메서드 추가

--- src/main/java/com/likelion/besession/domain/checklist/repository/CheckListRepository.java
+++ src/main/java/com/likelion/besession/domain/checklist/repository/CheckListRepository.java
@@ -12,4 +12,7 @@ public interface CheckListRepository extends JpaRepository<CheckList, Long> {
 
     // 계약 상태별 체크리스트 조회
     List<CheckList> findAllByContractAndPhase(Contract contract, ContractStatus phase);
+
+    // 현재 계약 단계에서 완료하지 않은 체크리스트가 있는지 확인
+    boolean existsByContractAndPhaseAndCheckListStatus(Contract contract, ContractStatus phase, CheckListStatus checkListStatus);
 }
